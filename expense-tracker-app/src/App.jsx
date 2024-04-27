ffbb954c5f7e607bb01357a8144b697d06ecbbd5
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-04-27 10:42:04 +0200

    Add form elements and inputs

--- expense-tracker-app/src/App.jsx
+++ expense-tracker-app/src/App.jsx
@@ -6,7 +6,28 @@ function App() {
 
   return (
     <>
-      
+      <form className="expense-form">
+        <h2>Add your expenses!</h2>
+        <label htmlFor="expense-title"><b>Expense title:</b></label>
+        <input type="text" name="title" required />
+
+        <label htmlFor="expense-amount"><b>Expense amount:</b></label>
+        <input type="text" name="amount" required />
+
+        <label htmlFor="expense-date"><b>Date of purchase:</b></label>
+        <input type="date" name="date" required />
+
+        <label htmlFor="expense-category"><b>Expense category:</b></label>
+        <select name="category" >
+        <option value="-">-</option>
+          <option value="Housing">Housing</option>
+          <option value="Grocery">Grocery</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Clothes">Clothes</option>
+          <option value="Other">Other</option>
+        </select>
+        
+      </form>
     </>
   )
 }
