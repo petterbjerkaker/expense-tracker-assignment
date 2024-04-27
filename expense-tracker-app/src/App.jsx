bed66615f617ec8f03fe3e6548a9010b25d35047
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-04-27 12:52:07 +0200

    Wrap html inside new classes

--- expense-tracker-app/src/App.jsx
+++ expense-tracker-app/src/App.jsx
@@ -25,60 +25,128 @@ class App extends React.Component {
       <>
         <div className="main-container">
           <ExpenseForm addExpense={this.addExpense} />
-          <ExpenseList expenses={this.state.expenses} onDelete={this.handleDeleteExpense}/>
+          <ExpenseList expenses={this.state.expenses} onDelete={this.handleDeleteExpense} />
         </div>
       </>
     );
   }
 }
-  return (
-   <>
-    <div className="main-container">
-      <form className="expense-form">
-        <h2>Add your expenses 📝</h2>
-        <label htmlFor="expense-title"><b>Expense title:</b></label>
-        <input type="text" name="title" required />
 
-        <label htmlFor="expense-amount"><b>Expense amount:</b></label>
-        <input type="text" name="amount" required />
+class ExpenseForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "",
+      amount: "",
+      date: "",
+      category: "-"
+    };
+  }
 
-        <label htmlFor="expense-date"><b>Date of purchase:</b></label>
-        <input type="date" name="date" required />
+  handleChange = (e) =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-        <label htmlFor="expense-category"><b>Expense category:</b></label>
-        <select name="category" >
-        <option value="-">-</option>
-          <option value="Housing">Housing</option>
-          <option value="Grocery">Grocery</option>
-          <option value="Transportation">Transportation</option>
-          <option value="Clothes">Clothes</option>
-          <option value="Other">Other</option>
-        </select>
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    const { title, amount, date, category } = this.state;
+    const newExpense = {
+      title,
+      amount,
+      date,
+      category
+    };
 
-        <button type="submit" className="submit-button">Add ➕</button>
-      </form>
+    this.props.addExpense(newExpense);
 
+    this.setState({
+      title: "",
+      amount: "",
+      date: "",
+      category:"-"
+    });
+  };
+
+
+  render(){
+    const { title, amount, date, category } = this.state;
+    return(
+      <form className="expense-form" onSubmit={this.handleSubmit}>
+      <h2>Add your expenses 📝</h2>
+      <label htmlFor="expense-title"><b>Expense title:</b></label>
+      <input type="text" name="title" value={title} onChange={this.handleChange} required />
+
+      <label htmlFor="expense-amount"><b>Expense amount:</b></label>
+      <input type="text" name="amount" value={amount} onChange={this.handleChange} required />
+
+      <label htmlFor="expense-date"><b>Date of purchase:</b></label>
+      <input type="date" name="date" value={date} onChange={this.handleChange} required />
+
+      <label htmlFor="expense-category"><b>Expense category:</b></label>
+      <select name="category" value={category} onChange={this.handleChange} >
+      <option value="-">-</option>
+        <option value="Housing">Housing</option>
+        <option value="Grocery">Grocery</option>
+        <option value="Transportation">Transportation</option>
+        <option value="Clothes">Clothes</option>
+        <option value="Other">Other</option>
+      </select>
+
+      <button type="submit" className="submit-button">Add ➕</button>
+    </form>
+    );
+  }
+}
+
+class ExpenseList extends React.Component{
+  handleDeleteExpense = (index) => {
+    const updatedExpenses = [...this.props.expenses];
+    updatedExpenses.splice(index, 1);
+    this.props.onDelete(updatedExpenses);
+  };
+
+  render(){
+    const { expenses } = this.props;
+    return(
       <section className="expense-list">
         <div className="display-expense-container">
-        <div className="display-expense">
-        <h2>Overview of expenses:</h2>
-        <ul>
-          <li>
-            <span><b>Title:</b></span>
-            <span><b>Amount:</b></span>
-            <span><b>Date:</b></span>
-            <span><b>Category:</b></span>
-            <button className="delete-button">Delete ❌</button>
-          </li>
-        </ul>
-      </div>
+          <DisplayExpenses expenses={expenses} onDelete={this.handleDeleteExpense} />
         </div>
       </section>
+    );
+  }
+}
 
-    </div>
-    </>
-  );
-};
+class DisplayExpenses extends React.Component{
+  handleDelete = (index)=>{
+    this.props.onDelete(index);
+  };
 
+  render(){
+    const { expenses } = this.props;
+    return(
+      <div className="display-expense">
+        <h2>Overview of expenses:</h2>
+        <ul>
+          {expenses.map((expense, index)=>(
+           <li key={index}>
+              <span><b>Title:</b>{expense.title}</span>
+              <span><b>Amount:</b>{expense.amount}</span>
+              <span><b>Date:</b>{expense.date}</span>
+              <span><b>Category:</b>{expense.category}</span>
+              <button 
+                className="delete-button" 
+                onClick={()=> 
+                this.handleDelete(index)}>Delete ❌
+              </button>
+           </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default App;
